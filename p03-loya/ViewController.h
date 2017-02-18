8b35b69035bf6d1b89cb0eb9168678b92bf42f22
Harshad Loya <harshadloya@Harshads-iMac.local>
2017-02-18 11:23:31 -0800

    Initial Commit

---
+++ p03-loya/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  p03-loya
+//
+//  Created by Harshad Loya on 2/18/17.
+//  Copyright © 2017 Harshad Loya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ p03-loya/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  p03-loya
+//
+//  Created by Harshad Loya on 2/18/17.
+//  Copyright © 2017 Harshad Loya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

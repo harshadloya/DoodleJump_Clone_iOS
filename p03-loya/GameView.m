7deacc6e066107172d9cd9e291e425e3ff79c599
Harshad Loya <harshadloya@Harshads-iMac.local>
2017-02-26 00:10:10 -0500

    updates

--- p03-loya/GameView.m
+++ p03-loya/GameView.m
@@ -35,9 +35,57 @@
 
 -(void)makeBricks:(id)sender
 {
+    UIImageView* animatedImageView = [[UIImageView alloc] initWithFrame:self.bounds];
+    
+    if([[Universe sharedInstance] levelCounter] == 1)
+    {
+        animatedImageView.animationImages = [NSArray arrayWithObjects: [UIImage imageNamed:@"noodleback.jpg"], nil] ;
+    }
+    else if ([[Universe sharedInstance] levelCounter] == 2)
+    {
+        animatedImageView.animationImages = [NSArray arrayWithObjects:
+                                             [UIImage imageNamed:@"space1.gif"], [UIImage imageNamed:@"space2.gif"],
+                                             [UIImage imageNamed:@"space3.gif"], [UIImage imageNamed:@"space4.gif"],
+                                             [UIImage imageNamed:@"space5.gif"], [UIImage imageNamed:@"space6.gif"],
+                                             [UIImage imageNamed:@"space7.gif"], [UIImage imageNamed:@"space8.gif"],
+                                             [UIImage imageNamed:@"space9.gif"], [UIImage imageNamed:@"space10.gif"],
+                                             [UIImage imageNamed:@"space11.gif"], [UIImage imageNamed:@"space12.gif"],
+                                             [UIImage imageNamed:@"space13.gif"], [UIImage imageNamed:@"space14.gif"],
+                                             [UIImage imageNamed:@"space15.gif"], [UIImage imageNamed:@"space16.gif"],
+                                             [UIImage imageNamed:@"space17.gif"], [UIImage imageNamed:@"space18.gif"],
+                                             [UIImage imageNamed:@"space19.gif"], [UIImage imageNamed:@"space20.gif"],
+                                             [UIImage imageNamed:@"space21.gif"], [UIImage imageNamed:@"space22.gif"],
+                                             [UIImage imageNamed:@"space23.gif"], [UIImage imageNamed:@"space24.gif"],
+                                             [UIImage imageNamed:@"space25.gif"], [UIImage imageNamed:@"space26.gif"],
+                                             [UIImage imageNamed:@"space27.gif"], [UIImage imageNamed:@"space28.gif"],
+                                             [UIImage imageNamed:@"space29.gif"], [UIImage imageNamed:@"space30.gif"],
+                                             [UIImage imageNamed:@"space31.gif"], [UIImage imageNamed:@"space32.gif"],
+                                             [UIImage imageNamed:@"space33.gif"], [UIImage imageNamed:@"space34.gif"],
+                                             [UIImage imageNamed:@"space35.gif"], [UIImage imageNamed:@"space36.gif"],
+                                             [UIImage imageNamed:@"space37.gif"], [UIImage imageNamed:@"space38.gif"], nil];
+    }
+    else if ([[Universe sharedInstance] levelCounter] == 3)
+    {
+        animatedImageView.animationImages = [NSArray arrayWithObjects:
+                                             [UIImage imageNamed:@"snow1.gif"], [UIImage imageNamed:@"snow2.gif"],
+                                             [UIImage imageNamed:@"snow3.gif"], [UIImage imageNamed:@"snow4.gif"],
+                                             [UIImage imageNamed:@"snow5.gif"], [UIImage imageNamed:@"snow6.gif"],
+                                             [UIImage imageNamed:@"snow7.gif"], [UIImage imageNamed:@"snow8.gif"],
+                                             [UIImage imageNamed:@"snow9.gif"], [UIImage imageNamed:@"snow10.gif"],
+                                             [UIImage imageNamed:@"snow11.gif"], [UIImage imageNamed:@"snow12.gif"],
+                                             [UIImage imageNamed:@"snow13.gif"], [UIImage imageNamed:@"snow14.gif"],
+                                             [UIImage imageNamed:@"snow15.gif"], [UIImage imageNamed:@"snow16.gif"],
+                                             [UIImage imageNamed:@"snow17.gif"], [UIImage imageNamed:@"snow18.gif"],
+                                             [UIImage imageNamed:@"snow19.gif"], [UIImage imageNamed:@"snow20.gif"], nil];
+    }
+    animatedImageView.animationDuration = 1.0f;
+    animatedImageView.animationRepeatCount = 0;
+    [animatedImageView startAnimating];
+    [self addSubview: animatedImageView];
+    
     CGRect bounds = [self bounds];
-    float width = 100;
-    float height = 167;
+    float width = 50;
+    float height = 10;
     
     srand(time(NULL));
     
@@ -56,20 +104,22 @@
         
         if([[Universe sharedInstance] levelCounter] == 1)
         {
-            [b setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"noodle.png"]]];
+            [b setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"noodlebrick.png"]]];
             [self setBackgroundColor:[UIColor purpleColor]];
             [_Level setText:@"Level 1"];
+            
+            
         }
         else if ([[Universe sharedInstance] levelCounter] == 2)
         {
-            [b setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"noodle.png"]]];
-            [self setBackgroundColor:[UIColor blueColor]];
+            [b setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"spacebrick.png"]]];
+            [self setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"spacebackground.gif"]]];
             [_Level setText:@"Level 2"];
         }
         else if ([[Universe sharedInstance] levelCounter] == 3)
         {
-            [b setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"noodle.png"]]];
-            [self setBackgroundColor:[UIColor lightGrayColor]];
+            [b setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"snowbrick.png"]]];
+            [self setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"snow.gif"]]];
             [_Level setText:@"Level 3"];
         }
         else
@@ -77,16 +127,21 @@
         
         [self addSubview:b];
         x = rand() % (int)(bounds.size.width * .8) + width/2;
-        NSLog(@"X %f", x);
+       // NSLog(@"X %f", x);
         
         y = (int)(bounds.size.height/11 * i + 37);
         
-        NSLog(@"Y %f", y);
+       // NSLog(@"Y %f", y);
         
         [b setCenter:CGPointMake(x , y)];
         [bricks addObject:b];
     }
-    
+   // Brick *c = [[Brick alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+    //[c setBackgroundColor:[UIColor blackColor]];
+    //[c setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"noodle.png"]]];
+    //[self addSubview:c];
+    //[c setCenter:CGPointMake(bounds.size.width/2 , bounds.size.height-150)];
+    //[bricks addObject:c];
 }
 
 -(void) startLevel
@@ -94,39 +149,40 @@
     CGRect bounds = [self bounds];
     
     
-    NSLog(@"Brick Point %f", bounds.size.width/2);
-    NSLog(@"Jumper Point %f", bounds.size.height - 50);
+    //NSLog(@"Brick Point %f", bounds.size.width/2);
+    //NSLog(@"Jumper Point %f", bounds.size.height-50);
     jumper = [[Jumper alloc] initWithFrame:CGRectMake(bounds.size.width/2, bounds.size.height-50, 50, 50)];
     
     [jumper setDx:0];
-    [jumper setDy:5];
+    [jumper setDy:10];
     
     if([[Universe sharedInstance] switchStatus])
     {
         if([[Universe sharedInstance] levelCounter] == 1)
-            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"snowman.png"]]];
+            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"ninja.png"]]];
         
         else if([[Universe sharedInstance] levelCounter] == 2)
             [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"astro.png"]]];
         
         else if([[Universe sharedInstance] levelCounter] == 3)
-            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"original.png"]]];
+            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"snowman.png"]]];
         
     }
     else
     {
         if([[Universe sharedInstance] levelCounter] == 1)
-            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"ironman.png"]]];
+            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"superman.png"]]];
         
         else if ([[Universe sharedInstance] levelCounter] == 2)
-            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"batman.png"]]];
+            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"ironman.png"]]];
         
         else if ([[Universe sharedInstance] levelCounter] == 3)
-            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"superman.png"]]];
+            [jumper setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"batman.png"]]];
     }
     
-    [self addSubview:jumper];
     [self makeBricks:nil];
+    [self addSubview:jumper];
+    
 }
 
 -(void)arrange:(CADisplayLink *)sender
@@ -152,10 +208,10 @@
     
     // If the jumper has fallen below the bottom of the screen,
     // add a positive velocity to move him
-    if (p.y > bounds.size.height-23)
+    if (p.y > bounds.size.height-25)
     {
-        [jumper setDy:5];
-        p.y = bounds.size.height-23;
+        [jumper setDy:10];
+        p.y = bounds.size.height-25;
     }
     
     // If we've gone past the top of the screen, wrap around
@@ -188,6 +244,7 @@
     
     // If we are moving down, and we touch a brick, we get
     // a jump to push us up.
+    //
     if ([jumper dy] < 0)
     {
         for (Brick *brick in bricks)
@@ -195,8 +252,10 @@
             CGRect b = [brick frame];
             //NSLog(@" %d   %d", b, p);
             
-            p.y -= 23;
+            p.y += 17;
             if (CGRectContainsPoint(b, p))
+            //CGPoint temp = [brick center];
+            //if(temp.x == p.x && temp.y == p.y)
             {
              //   NSLog(@"Brick Point %@", NSStringFromCGRect(b));
                // NSLog(@"Jumper Point %@", NSStringFromCGPoint(p));
@@ -204,9 +263,10 @@
                 NSLog(@"Bounce!");
                 [jumper setDy:10];
             }
-            p.y += 23;
+            p.y -= 17;
         }
     }
+    //p.y -= 117;
     
     [jumper setCenter:p];
     // NSLog(@"Timestamp %f", ts);
